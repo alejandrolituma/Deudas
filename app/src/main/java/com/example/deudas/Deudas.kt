c88b321b21c48f81eac4435ac2f358eaa04c43f9
Alejandro Lituma <miguel.lituma@oracle.com>
2020-01-31 15:57:27 -0600

    Initial Commit

---
+++ app/src/main/java/com/example/deudas/Deudas.kt
@@ -0,0 +1,12 @@
+package com.example.deudas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Deudas : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_deudas)
+    }
+}
